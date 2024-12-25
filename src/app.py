7c9c5f0c1e160dfab60c121d8e80d99acd5d05b1
Gelzieny R. Martins <gelzieny@gmail.com>
2024-12-25 15:39:47 -0300

    criando função de DELETE e realizando atualização na rota de consulta de todos os usuários cadastrados

--- src/app.py
+++ src/app.py
@@ -9,6 +9,7 @@ from database import db  # Certifique-se de que o caminho de importação está 
 from models.user import User  # Certifique-se de que o caminho de importação está correto
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash  # Para verificar e gerar senhas criptografadas
+from sqlalchemy.exc import SQLAlchemyError
 
 # Criação da aplicação Flask
 app = Flask(__name__)
@@ -78,14 +79,67 @@ def register():
 
   return jsonify({"message": "Dados inválidos"}), 400
 
+
+@app.route('/list-user', methods=["GET"])
+@login_required  
+def get_user():
+  users = User.query.all()
+
+  if not users:
+    return jsonify({"message": "Nenhum usuário encontrado"}), 404
+
+  users_data = []
+  for user in users:
+    users_data.append({
+      "id": user.id,
+      "username": user.username
+    })
+
+  return jsonify(users_data), 200
+
+@app.route('/update-user', methods=["PUT"])
+@login_required
+def update_user():
+  data = request.json
+
+  if "username" in data:
+    current_user.username = data["username"]
+
+  if "password" in data:
+    current_user.password = generate_password_hash(data["password"]) 
+
+  db.session.commit()
+
+  return jsonify({"message": "Dados atualizados com sucesso!"}), 200
+
 @app.route('/logout', methods=["GET"])
-@login_required  # Garante que apenas usuários autenticados possam fazer logout
+@login_required 
 def logout():
   if not current_user.is_authenticated:
     return jsonify({"message": "Você precisa estar autenticado para fazer logout."}), 401
   
-  logout_user()  # Encerra a sessão do usuário
+  logout_user()
   return jsonify({"message": "Desconectado com sucesso!"}), 200
+
+@app.route('/delete-user/<int:user_id>', methods=["DELETE"])
+@login_required
+def delete_user(user_id):
+
+  if user_id == current_user.id:
+    return jsonify({"message": "Você não pode excluir sua própria conta!"}), 400
+
+  user = User.query.get(user_id)
+  
+  if not user:
+    return jsonify({"message": "Usuário não encontrado"}), 404
+
+  try:
+    db.session.delete(user) 
+    db.session.commit() 
+    return jsonify({"message": "Usuário deletado com sucesso!"}), 200
+  except SQLAlchemyError as e:
+    db.session.rollback() 
+    return jsonify({"message": f"Erro ao deletar o usuário: {str(e)}"}), 500
 
 if __name__ == '__main__':
   with app.app_context():
